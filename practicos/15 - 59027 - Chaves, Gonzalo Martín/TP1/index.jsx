c5e2aa4c2ca95975f60d298fb0b4f46d2bcf94f1
Gonzalo Chaves <167923368+GonzaCarp@users.noreply.github.com>
2024-05-01 19:04:50 -0300

    Gonzalo Chaves TP1

--- practicos/15 - 59027 - Chaves, Gonzalo Martín/TP1/index.jsx
+++ practicos/15 - 59027 - Chaves, Gonzalo Martín/TP1/index.jsx
@@ -1,8 +1,38 @@
-const App = () =>  (
-    <div>
-        <h1>¡Hola, mundo!</h1>
-        <p>¡Bienvenidos a React!</p>
-    </div>
-)
+const contactos = [
+{id:10, nombre: "Gonzalo ", apellido: "Chaves", Telefono: "381-6770006"},
+{id:20, nombre: "Amir ", apellido: "Chein", Telefono: "381-4133467"},
+{id:30, nombre: "Santiago ", apellido: "Chein", Telefono: "381-6125880"},
+{id:40, nombre: "Juan ", apellido: "Coronel", Telefono: "381-5853419"},
+{id:50, nombre: "Gustavo ", apellido: "Cecilia", Telefono: "381-5245764"},
+];
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
+const Contacto = ({id, nombre, apellido, telefono})=>
+(
+    <div className="cont">
+        <div className="datos">
+        <h3 id="id2">{id} <br/></h3>
+        <h3 id="nombre">{apellido} {nombre} </h3>
+        <h3 id="telefono">{telefono}</h3>
+        </div>
+    </div>
+);
+
+
+const App = () =>  (
+    <div className="principal">
+        <h1>TARJETAS DE CONTACTOS!</h1>     
+        {
+        contactos.map(contactos => (
+        <Contacto
+            id = {contactos.id}
+            nombre = {contactos.nombre}
+            apellido = {contactos.apellido}
+            telefono = {contactos.Telefono}
+        />))
+        }
+    </div>
+);
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
