6d7be406da480714c1972b7c858595bfb55307ea
matias22lucena <matias22lucena@gmail.com>
2024-05-01 13:15:36 -0300

    Trabajo Practico N°1 Hecho

--- practicos/38 - 59184 - Lucena, Matias Leandro/TP1/index.jsx
+++ practicos/38 - 59184 - Lucena, Matias Leandro/TP1/index.jsx
@@ -1,8 +1,31 @@
-const App = () =>  (
+const titulos = () =>  (
     <div>
-        <h1>¡Hola, mundo!</h1>
-        <p>¡Bienvenidos a React!</p>
+        <h1>Agenda</h1>
     </div>
 )
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const Contacto = ({nombre, apellido, teléfono}) => (
+    <div className="Contactos">
+        <h2>{nombre} <b>{apellido.toUpperCase()}</b></h2>
+        <p>Telefono: {teléfono}</p>
+    </div>
+)
+
+
+const Agenda = () => (
+    <div className="div">
+        <h1>AGENDA</h1>
+        <Contacto nombre = "Matias"     apellido= "Lucena"   teléfono= "3813156301"/>
+        <Contacto nombre = "Julio"      apellido= "Lucena"   teléfono= "3815711858"/>
+        <Contacto nombre = "Carina"     apellido= "Elias"    teléfono= "3814635690"/>
+        <Contacto nombre = "Sofia"      apellido= "Lucena"   teléfono= "3816189852"/>
+        <Contacto nombre = "Emiliano"   apellido= "Lucena"   teléfono= "3812080347"/>
+        <Contacto nombre = "Javier"     apellido= "Lucena"   teléfono= "3815619317"/>
+        <Contacto nombre = "Agustin"    apellido= "Elias"    teléfono= "2964529741"/>
+    </div>
+)
+
+
+
+
+ReactDOM.render(<Agenda />, document.getElementById('root'))
