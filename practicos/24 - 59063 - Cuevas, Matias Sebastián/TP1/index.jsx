82c2861a66b4d1c32207a9c45da2b4d18e1ae444
matia415 <matiascuevas2017@gmail.com>
2024-05-01 18:08:16 -0300

    tp1
    trabajo practico numero º1

--- practicos/24 - 59063 - Cuevas, Matias Sebastián/TP1/index.jsx
+++ practicos/24 - 59063 - Cuevas, Matias Sebastián/TP1/index.jsx
@@ -1,37 +1,31 @@
-
-
-const Contactos=()=>
-[
-     {id:1, nombre:"", apelllido: "",  telefono, email:""},
-      
-]
-
   
-const Agenda=({id,nombre,apelllido,telefono,email})=>
+const Agenda=(props)=>
 
 (
-    
+
     <div className="agenda"> 
-      <h4>{nombre} <b>{apelllido.toUpperCase()}</b></h4>
-      <p>telefono: {telefono}</p>
-      <p>email:{email}</p>
+      <ul className="lista">
+        <li><b>nombre</b>:{props.nombre},{props.apelllido}</li>
+        <li><b>telefono</b>:{props.telefono}</li>
+        <li> <b>email</b>:{props.email}</li> 
+      </ul>
+     
         </div>
 
 )
 
 
 const App = ({id,nombre,apelllido,telefono,email}) =>  (
-    <div className="agenda">
-    <h1 id="titulo" className="titulo" >Agenda de Contactos</h1>
+
   
+    <div className="agenda">
+   <div className="nombre"> <h1>contactos</h1> </div>
     <br />
-    <br />
-    <br />
-    <br />
+    
      < Agenda  nombre="Matias" apelllido="cuevas" telefono="3813437567" email="matiascuevas2017@gmail.com" />
-     < Agenda  nombre="Sebastian" apelllido="cruz" telefono="387 503-6525"  email="sebas200@gmail.com" />
-     < Agenda  nombre="Mario" apelllido="Corral" telefono="387 502-6526" email="mariocorral@gmail.com" />
-     < Agenda  nombre="Franco" apelllido="Catania" telefono="3876 47-6622 "email="franco@mail.com" />,
+     < Agenda  nombre="Sebastian" apelllido="cruz" telefono="3875036525"  email="sebas200@gmail.com" />
+     < Agenda  nombre="Mario" apelllido="Corral" telefono="3875026526" email="mariocorral@gmail.com" />
+     < Agenda  nombre="Franco" apelllido="Catania" telefono="3876476622 "email="franco@mail.com" />,
   
 
 
