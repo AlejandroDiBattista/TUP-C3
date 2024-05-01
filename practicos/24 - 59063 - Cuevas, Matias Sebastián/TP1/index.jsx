4c2a55738ff66342a436271cb7802c994b21a6cc
Alejandro Di Battista <alejandrodibattista@gmail.com>
2024-05-01 01:29:16 -0300

    .

--- practicos/24 - 59063 - Cuevas, Matias Sebastián/TP1/index.jsx
+++ practicos/24 - 59063 - Cuevas, Matias Sebastián/TP1/index.jsx
@@ -1,40 +1,8 @@
-
-const Contacto = ({nombre, apellido, telefono}) => (
+const App = () =>  (
     <div>
-        <h2>Contacto</h2>
-        <p>Nombre: {nombre}</p>
-        <p>Apellido: {apellido}</p>
-        <p>Telefono: {telefono}</p>
+        <h1>¡Hola, mundo!</h1>
+        <p>¡Bienvenidos a React!</p>
     </div>
 )
 
-class Persona {
-    constructor(nombre, apellido, telefono) {
-        this.nombre = nombre
-        this.apellido = apellido
-        this.telefono = telefono
-    }
-}
-
-let agenda = [
-    new Persona("Matias", "Cuevas", 123456789),
-    new Persona("Juan", "Perez", 987654321),
-    new Persona("Maria", "Gomez", 456789123),
-    { nombre: "Juan", apellido: "Perez", telefono: 987654321 },
-]
-
-
-const App = () => (
-    < div >
-    {
-        agenda.map((contacto, index) => (
-            <Contacto key={index} nombre={contacto.nombre} apellido={contacto.apellido} telefono={contacto.telefono} />
-        ))
-    }
-    </div >
-)
-
-[1, 2, 3, 4].map(x => x * 10) //> [10, 20, 30, 40]
-agenda.map(contacto => Contacto(contacto))
-agenda.map(contacto.)
 ReactDOM.render(<App />, document.getElementById('root'))
