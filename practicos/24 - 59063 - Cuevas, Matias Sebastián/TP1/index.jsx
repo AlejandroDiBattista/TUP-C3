fa0e2c6f2e7f5bebc3e03847ac6db378970103aa
matia415 <matiascuevas2017@gmail.com>
2024-04-30 23:55:33 -0300

    tpp1
    proyecto terminado

--- practicos/24 - 59063 - Cuevas, Matias Sebastián/TP1/index.jsx
+++ practicos/24 - 59063 - Cuevas, Matias Sebastián/TP1/index.jsx
@@ -1,8 +1,42 @@
-const App = () =>  (
-    <div>
-        <h1>¡Hola, mundo!</h1>
-        <p>¡Bienvenidos a React!</p>
+
+
+const Contactos=()=>
+[
+     {id:1, nombre:"", apelllido: "",  telefono, email:""},
+      
+]
+
+  
+const Agenda=({id,nombre,apelllido,telefono,email})=>
+
+(
+    
+    <div className="agenda"> 
+      <h4>{nombre} <b>{apelllido.toUpperCase()}</b></h4>
+      <p>telefono: {telefono}</p>
+      <p>email:{email}</p>
+        </div>
+
+)
+
+
+const App = ({id,nombre,apelllido,telefono,email}) =>  (
+    <div className="agenda">
+    <h1 id="titulo" className="titulo" >Agenda de Contactos</h1>
+  
+    <br />
+    <br />
+    <br />
+    <br />
+     < Agenda  nombre="Matias" apelllido="cuevas" telefono="3813437567" email="matiascuevas2017@gmail.com" />
+     < Agenda  nombre="Sebastian" apelllido="cruz" telefono="387 503-6525"  email="sebas200@gmail.com" />
+     < Agenda  nombre="Mario" apelllido="Corral" telefono="387 502-6526" email="mariocorral@gmail.com" />
+     < Agenda  nombre="Franco" apelllido="Catania" telefono="3876 47-6622 "email="franco@mail.com" />,
+  
+
+
     </div>
 )
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
