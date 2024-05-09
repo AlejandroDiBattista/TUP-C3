d2a93ff6c2b101f68077f1e18fe72900b4f91359
ProgramadorPromedio02 <137217436+ProgramadorPromedio02@users.noreply.github.com>
2024-05-09 15:05:21 -0300

    TP1 Fixed 09-05-24

--- practicos/34 - 55249 - Fernandez, Gonzalo Tomas/TP1/index.jsx
+++ practicos/34 - 55249 - Fernandez, Gonzalo Tomas/TP1/index.jsx
@@ -1,72 +1,69 @@
-const Title = () => <h1>🕵️‍♂️Mi Agenda🕵️‍♂️</h1>;
-
-const Header = () => (
-  <div id="title">
-    <Title />
-  </div>
-);
-
-const Contacts = () => [
-  { id: 1, nombre: 'Julian', apellido: 'Rodrigéz', telefono: '381-456-7890' },
-  { id: 2, nombre: 'Karen', apellido: 'Gonzaléz', telefono: '381-567-8901' },
-  { id: 3, nombre: 'Gabriel', apellido: 'Santillan', telefono: '381-678-9012' },
-  { id: 4, nombre: 'Lourdes', apellido: 'Olea', telefono: '381-789-0123' },
-  { id: 5, nombre: 'Macarena', apellido: 'Sánchez', telefono: '381-890-1234' },
-  { id: 6, nombre: 'Lourdes', apellido: 'Rodrigéz', telefono: '381-901-2345' },
-  { id: 7, nombre: 'Gabriel', apellido: 'Isa', telefono: '381-012-3456' },
-  { id: 8, nombre: 'Barbara', apellido: 'Santillan', telefono: '381-123-4567' },
-  { id: 9, nombre: 'Leandro', apellido: 'Ausar', telefono: '381-234-5678' },
-  { id: 10, nombre: 'Valentin', apellido: 'Giménez', telefono: '381-345-6789' },
+const data = [
+  { id: 1, username: 'Julian', lastname: 'Rodrigéz', phone: '(381) 456-7890' },
+  { id: 2, username: 'Karen', lastname: 'Gonzaléz', phone: '(381) 567-8901' },
+  {
+    id: 3,
+    username: 'Gabriel',
+    lastname: 'Santillan',
+    phone: '(381) 678-9012',
+  },
+  { id: 4, username: 'Lourdes', lastname: 'Olea', phone: '(381) 789-0123' },
+  { id: 5, username: 'Macarena', lastname: 'Sánchez', phone: '(381) 890-1234' },
+  { id: 6, username: 'Lourdes', lastname: 'Rodrigéz', phone: '(381) 901-2345' },
+  { id: 7, username: 'Gabriel', lastname: 'Isa', phone: '(381) 012-3456' },
+  {
+    id: 8,
+    username: 'Barbara',
+    lastname: 'Santillan',
+    phone: '(381) 123-4567',
+  },
+  { id: 9, username: 'Leandro', lastname: 'Ausar', phone: '(381) 234-5678' },
+  {
+    id: 10,
+    username: 'Valentin',
+    lastname: 'Giménez',
+    phone: '(381) 345-6789',
+  },
 ];
 
-const Contact = ({ nombre, apellido, telefono }) => (
+const Contact = ({ username, lastname, phone }) => (
   <tr>
-    <td>{nombre}</td>
-    <td>{apellido}</td>
-    <td>{telefono}</td>
+    <td>{username}</td>
+    <td>{lastname}</td>
+    <td>{phone}</td>
   </tr>
 );
 
-const Content = () => (
-  <div className="contact">
-    <table>
-      <thead>
-        <tr>
-          <th>Nombre</th>
-          <th>Apellido</th>
-          <th>Teléfono</th>
-        </tr>
-      </thead>
-      <tbody>
-        {Contacts().map((contact) => (
-          <Contact
-            key={contact.id}
-            nombre={contact.nombre}
-            apellido={contact.apellido}
-            telefono={contact.telefono}
-          />
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
-
-const Footer = () => (
-  <div className="footer">
-    <footer>@Gonzalo Tomas Fernandez</footer>
-  </div>
-);
-
-const Agenda = () => {
-  return (
-    <div>
-      <Header />
-      <Content />
-      <Footer />
+const App = () => (
+  <div>
+    <header>
+      <h1 className="title">🕵️‍♂️ Mi Agenda 🕵️‍♂️</h1>
+    </header>
+    <div className="contact">
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Teléfono</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((contact) => (
+            <Contact
+              key={contact.id}
+              username={contact.username}
+              lastname={contact.lastname}
+              phone={contact.phone}
+            />
+          ))}
+        </tbody>
+      </table>
     </div>
-  );
-};
-
-const App = () => <Agenda />;
+    <footer className="footer">
+      <p>@Gonzalo Tomas Fernandez</p>
+    </footer>
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
