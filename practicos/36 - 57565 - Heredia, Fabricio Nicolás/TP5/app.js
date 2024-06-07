520c4e0e41bcab5aa0110b2bf546a8aaf6a8eb14
fabricio-heredia <fabricioheredia9912@gmail.com>
2024-06-07 15:17:37 -0300

    tp5-FabricioHeredia

--- practicos/36 - 57565 - Heredia, Fabricio Nicolás/TP5/app.js
+++ practicos/36 - 57565 - Heredia, Fabricio Nicolás/TP5/app.js
@@ -7,15 +7,49 @@ app.use(express.json());
 app.use(cors())
 
 let datos = [
-    // Datos de ejemplo   
+    { id: 1, nombre: "Raul", apellido: "Lopez", edad: 28, borrado: false, actualizado: Date.now() },
+    { id: 2, nombre: "Marcela", apellido: "Gonzales", edad: 39, borrado: false, actualizado: Date.now() },
+    { id: 3, nombre: "Lucas", apellido: "Fernandez", edad: 25, borrado: false, actualizado: Date.now() },
+    { id: 4, nombre: "Maria", apellido: "Disalvo", edad: 18, borrado: false, actualizado: Date.now() },
+    { id: 5, nombre: "Mario", apellido: "correa", edad: 36, borrado: false, actualizado: Date.now()}
 ]
 
 app.get('/personas', (req, res) => {
-    // Implementar GET_ALL
+    res.status(200).json(datos.filter(persona => !persona.borrado));
 });
 
 app.put('/personas', (req, res) => {
-    // Implementar PUT
-})
+    const { id, nombre, apellido, edad, borrado } = req.body;
+    if (id) {
+        const personaIndex = datos.findIndex(persona => persona.id === id);
 
+        if (personaIndex === -1) {
+            return res.status(404).json({ mensaje: 'Persona no encontrada' });
+        }
+
+        datos[personaIndex] = {
+            ...datos[personaIndex],
+            nombre: nombre || datos[personaIndex].nombre,
+            apellido: apellido || datos[personaIndex].apellido,
+            edad: edad || datos[personaIndex].edad,
+            borrado: borrado !== undefined ? borrado : datos[personaIndex].borrado,
+            actualizado: Date.now()
+        };
+
+        return res.status(201).json(datos[personaIndex]);
+    } else {
+        const newPersona = {
+            id: datos.length ? Math.max(...datos.map(p => p.id)) + 1 : 1,
+            nombre,
+            apellido,
+            edad,
+            borrado: false,
+            actualizado: Date.now()
+        };
+
+        datos.push(newPersona);
+
+        res.status(201).json({ id: newPersona.id });
+    }
+});
 export default app
