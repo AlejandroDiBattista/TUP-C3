2819da6ba0ecc35a21489068bf61f3684d6f2fb4
Daz0600 <diaznahuel0600@gmail.com>
2024-06-08 02:18:43 -0300

    TP5

--- practicos/29 - 58844 - Diaz Gonzalez, Nahuel Ale/TP5/app.js
+++ practicos/29 - 58844 - Diaz Gonzalez, Nahuel Ale/TP5/app.js
@@ -1,21 +1,48 @@
 import express from 'express'
 import cors from 'cors'
-
 const app = express()
-
 app.use(express.json());
 app.use(cors())
 
 let datos = [
-    // Datos de ejemplo   
+    { actualizado: 0, apellido: "Perez", borrado: false, edad: 30, id: 0, nombre: "Juana" },
+    { actualizado: 0, apellido: "Gonzalez", borrado: false, edad: 25, id: 1, nombre: "Luis" },
+    { actualizado: 0, apellido: "Lopez", borrado: false, edad: 30, id: 2, nombre: "Juan" },
+    { actualizado: 0, apellido: "Gonzalez", borrado: false, edad: 25, id: 3, nombre: "María" },
+    { actualizado: 0, apellido: "Perez", borrado: false, edad: 30, id: 4, nombre: "María" }
 ]
 
 app.get('/personas', (req, res) => {
-    // Implementar GET_ALL
+    try {
+        const datosReales = datos.filter(dato => !dato.borrado)
+        res.send(datosReales)
+    } catch (error) {
+        console.log('error en GET /personas', error)
+        res.status(500).json({ error: "Error interno del servidor" })
+    }
 });
 
 app.put('/personas', (req, res) => {
-    // Implementar PUT
-})
+    try {
+        const dato = req.body;
+
+        if (dato.id === undefined) {
+            dato.id = datos.length > 0 ? datos[datos.length - 1].id + 1 : 1
+            datos.push(dato)
+            res.status(201).json(dato)
+        } else {
+            const index = datos.findIndex(p => p.id === dato.id)
+            if (index !== -1) {
+                datos[index] = { ...datos[index], ...dato }
+                res.status(201).json(datos[index])
+            } else {
+                res.status(404).json({ message: 'Dato no encontrado' })
+            }
+        }
+    } catch (error) {
+        console.error('Error en PUT /personas:', error)
+        res.status(500).json({ error: 'Error interno del servidor' })
+    }
+});
 
 export default app
