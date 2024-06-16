36c7d08262e9b06d18e9fa3d0359914b10471a29
ProgramadorPromedio02 <137217436+ProgramadorPromedio02@users.noreply.github.com>
2024-06-16 12:37:45 -0300

    TP6-fernandez-gonzalo-tomas

--- practicos/34 - 55249 - Fernandez, Gonzalo Tomas/TP6/servidor.js
+++ practicos/34 - 55249 - Fernandez, Gonzalo Tomas/TP6/servidor.js
@@ -1,15 +1,20 @@
+// servidor.js
+
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import morgan
- from 'morgan';
+import morgan from 'morgan';
+import User from './routers/user.router.js';
+
 const app = express();
 
-app.use(morgan('dev'));     // Loggea cada request en consola
+app.use(morgan('dev'));     // Logea cada request en consola
 app.use(cookieParser());    // Para leer cookies
 app.use(express.json());    // Para leer JSONs
 app.use(express.static('public'));  // Para servir archivos estáticos
+app.disable('x-powered-by');
 
-// Implementar las rutas necesarias
+app.use(User);
+
 app.listen(3000, () => {
-    console.log('Servidor iniciado en http://localhost:3000');
+  console.log('Servidor corriendo en http://localhost:3000');
 });
