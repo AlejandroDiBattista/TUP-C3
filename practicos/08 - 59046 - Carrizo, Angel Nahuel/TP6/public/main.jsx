5b6ee177023a0f18c28e649e82059ab716aeb56b
NaCroAz <nahuel.ac12@gmail.com>
2024-06-21 20:32:47 -0300

    Tp6-Carrizo_Nahuel

--- practicos/08 - 59046 - Carrizo, Angel Nahuel/TP6/public/main.jsx
+++ practicos/08 - 59046 - Carrizo, Angel Nahuel/TP6/public/main.jsx
@@ -1,7 +1,186 @@
 function App() {
+    const [registroData, setRegistroData] = useState({ usuario: '', password: '' });
+    const [loginData, setLoginData] = useState({ usuario: '', password: '', token: '' });
+    const [mensajeError, setMensajeError] = useState('');
+    const [mensajeExito, setMensajeExito] = useState('');
+    const [mensajeInformacion, setMensajeInformacion] = useState({ mensaje: '', usuario: '' });
+
+    const generateToken = () => {
+        const token = Math.random().toString(36).substring(2);
+        return token;
+    };
+
+    const handleRegistroChange = (e) => {
+        const { name, value } = e.target;
+        setRegistroData({ ...registroData, [name]: value });
+        console.log(name, value);
+    };
+
+    const handleLoginChange = (e) => {
+        const { name, value } = e.target;
+        setLoginData({ ...loginData, [name]: value });
+        console.log(name, value);
+    };
+
+    const registerUser = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('/register', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(registroData)
+            });
+            const data = await response.json();
+            console.log(data);
+
+            if (data.ok) {
+                setMensajeExito(data.mensaje);
+                setMensajeError('');
+            } else {
+                setMensajeError(data.mensaje);
+                setMensajeExito('');
+            }
+        } catch (error) {
+            console.error('Error al registrar usuario:', error);
+        }
+    };
+
+    const loginUser = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('/login', {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(loginData)
+            });
+
+            const data = await response.json();
+            console.log(data);
+
+            if (data.usuario !== undefined && data.password !== undefined) {
+                // Generar el token solo si usuario y password no son indefinidos
+                const token = generateToken();
+                setLoginData({ ...loginData, token });
+                setMensajeExito(data.mensaje);
+                setMensajeError('');
+            } else {
+                setMensajeError(data.mensaje);
+                setMensajeExito('');
+            }
+        } catch (error) {
+            console.error('Error al iniciar sesión:', error);
+        }
+    };
+
+    const fetchInfo = async () => {
+        try {
+            const response = await fetch('/info', {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' }
+            });
+
+            const data = await response.json();
+            console.log(data);
+
+            if (data.ok) {
+                const newInfo = { mensaje: data.mensaje, usuario: data.usuario };
+                setMensajeInformacion(newInfo);
+            } else {
+                setMensajeError(data.mensaje);
+                setMensajeExito('');
+            }
+        } catch (error) {
+            console.error('Error al obtener información del usuario:', error);
+        }
+    };
+
+    const logoutUser = async () => {
+        try {
+            const response = await fetch('/logout', {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' }
+            });
+
+            const data = await response.json();
+            console.log(data);
+            setLoginData({ ...loginData, token: '' });
+            setMensajeExito(data.mensaje);
+            setMensajeError('');
+        } catch (error) {
+            console.error('Error al cerrar sesión:', error);
+        }
+    };
+
     return (
-        <div>
-            <h1>TP6 - Sesiones</h1>
+        <div className="form-container">
+            <h1 className="header">Gestion de sesiones</h1>
+            <div className="form-section-one">
+                <div className="registration-form">
+                    <h3 className="form-header">Registro</h3>
+                    <form onSubmit={registerUser}>
+                        <div className="form-inputs">
+                            <input
+                                type="text"
+                                name="usuario"
+                                placeholder="Crear usuario"
+                                value={registroData.usuario}
+                                onChange={handleRegistroChange}
+                            />
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Crear contraseña"
+                                value={registroData.password}
+                                onChange={handleRegistroChange}
+                            />
+                        </div>
+                        <button type="submit">Registrar</button>
+                    </form>
+                </div>
+                <div className="login-form">
+                    <h3 className="form-header">Iniciar Sesión</h3>
+                    <form onSubmit={loginUser}>
+                        <div className="form-inputs">
+                            <input
+                                type="text"
+                                name="usuario"
+                                placeholder="Ingresar usuario"
+                                value={loginData.usuario}
+                                onChange={handleLoginChange}
+                            />
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Ingresar contraseña"
+                                value={loginData.password}
+                                onChange={handleLoginChange}
+                            />
+                        </div>
+                        <button type="submit">Iniciar Sesión</button>
+                    </form>
+                </div>
+            </div>
+            {loginData.token === '' ? (
+                <div className="form-section-two">
+                        <p>Inicia sesión para ver esta información</p>
+                </div>
+            ) : (
+                <div className="form-section-two">
+                    <div className="user-info">
+                        <p>{mensajeInformacion.mensaje + " " + mensajeInformacion.usuario}</p>
+                    </div>
+                    <div className="action-buttons">
+                        <button onClick={fetchInfo}>Ver info</button>
+                        <button onClick={logoutUser}>Cerrar Sesión</button>
+                    </div>
+                    <div className="messages">
+                        <p className="error-message">{mensajeError}</p>
+                        <p className="success-message">{mensajeExito}</p>
+                    </div>
+                </div>
+            )}
         </div>
-    )
+    );
 }
--- practicos/08 - 59046 - Carrizo, Angel Nahuel/TP6/servidor.js
+++ practicos/08 - 59046 - Carrizo, Angel Nahuel/TP6/servidor.js
@@ -1,15 +1,124 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import morgan
- from 'morgan';
+import morgan from 'morgan';
+
 const app = express();
 
-app.use(morgan('dev'));     // Loggea cada request en consola
-app.use(cookieParser());    // Para leer cookies
-app.use(express.json());    // Para leer JSONs
-app.use(express.static('public'));  // Para servir archivos estáticos
+app.use(morgan('dev')); // Loggea cada request en consola
+app.use(cookieParser()); // Para leer cookies
+app.use(express.json()); // Para leer JSONs
+app.use(express.static('public')); // Para servir archivos estáticos
 
-// Implementar las rutas necesarias
+// Lista de usuarios
+const usuariosLista = [
+    { usuario: 'admin', password: 'admin' },
+];
+
+// Rutas
+
+// Obtener lista de usuarios (solo para pruebas, no en producción)
+app.get('/users', (req, res) => {
+    res.json(usuariosLista);
+});
+
+// Registrarse
+app.post('/register', (req, res) => {
+    const { usuario, password } = req.body;
+    console.log(usuario, password);
+
+    if (!usuario || !password) {
+        res.status(400).json({ error: 'Faltan datos' });
+        alert('Faltan datos');
+        return;
+    }
+
+    const usuarioEncontrado = usuariosLista.find(u => u.usuario === usuario);
+
+    if (usuarioEncontrado) {
+        res.status(400).json({ error: 'El usuario ya existe' });
+        alert('El usuario ya existe');
+        return;
+    } else {
+        usuariosLista.push({ usuario, password });
+        res.status(201).json({ message: 'Usuario creado' });
+        alert('Usuario creado');
+    }
+});
+
+// Iniciar sesión
+app.put('/login', (req, res) => {
+    const { usuario, password } = req.body;
+
+    if (!usuario || !password) {
+        res.status(400).json({ error: 'Faltan datos' });
+        alert('Faltan datos');
+        return;
+    }
+
+    const usuarioEncontrado = usuariosLista.find(u => u.usuario === usuario && u.password === password);
+
+    if (usuarioEncontrado) {
+        const token = Math.random().toString(36).substring(2);
+        usuarioEncontrado.token = token;
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            expires: new Date(Date.now() + 60 * 60 * 1000), // 1 hora
+        });
+
+        res.status(200).json({ message: 'Sesión iniciada', usuario, password });
+        alert('Sesión iniciada');
+    } else {
+        res.status(400).json({ error: 'Credenciales incorrectas' });
+        alert('Credenciales incorrectas');
+    }
+});
+
+// Deslogearse
+app.get('/logout', (req, res) => {
+    const { token } = req.cookies;
+
+    if (!token) {
+        res.status(400).json({ error: 'No hay sesión activa' });
+        alert('No hay una sesión activa');
+        return;
+    }
+
+    const usuario = usuariosLista.find(u => u.token === token);
+
+    if (usuario) {
+        delete usuario.token;
+        res.clearCookie('token');
+        res.status(200).json({ message: 'Sesión cerrada' });
+        alert('Sesión cerrada');
+    } else {
+        res.status(400).json({ error: 'No hay sesión activa' });
+        alert('No hay una sesión activa');
+    }
+});
+
+// Información del usuario
+app.get('/info', (req, res) => {
+    const { token } = req.cookies;
+
+    if (!token) {
+        res.status(401).json({ error: 'No hay una sesión activa' });
+        alert('No hay una sesión activa');
+        return;
+    }
+
+    const usuario = usuariosLista.find(u => u.token === token);
+
+    if (!usuario) {
+        res.status(401).json({ error: 'No hay una sesión activa' });
+        alert('No hay una sesión activa');
+        return;
+    } else {
+        res.json({ mensaje: 'Información del usuario', usuario: usuario.usuario });
+    }
+});
+
+// Iniciar el servidor
 app.listen(3000, () => {
     console.log('Servidor iniciado en http://localhost:3000');
 });
